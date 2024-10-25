5de3c9b2dc3895335c42f172a9fbcb4aaed48e3c
Maikel <230254@fppiramide.com>
2024-10-25 10:50:33 +0200

    PROG: proyecto añadido

---
+++ src/main/java/com/boley/boley/BoleyApplication.java
@@ -0,0 +1,13 @@
+package com.boley.boley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boley/boley/BoleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boley.boley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
